046a304c7fd07708143c32e7901a7b929ef1b01e
maramosh <ma.ramosh@uniandes.edu.co>
2022-03-10 20:32:58 -0500

    Build para producción

---
+++ build/precache-manifest.a7bc34af03c1b576ec9d890628e833fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b97b52fcf9b05bb7ac45",
+    "url": "/pokehoum-challenge/static/css/main.7d79da71.chunk.css"
+  },
+  {
+    "revision": "b97b52fcf9b05bb7ac45",
+    "url": "/pokehoum-challenge/static/js/main.b97b52fc.chunk.js"
+  },
+  {
+    "revision": "e127c28baf869c0882da",
+    "url": "/pokehoum-challenge/static/css/1.a1cb50fa.chunk.css"
+  },
+  {
+    "revision": "e127c28baf869c0882da",
+    "url": "/pokehoum-challenge/static/js/1.e127c28b.chunk.js"
+  },
+  {
+    "revision": "4acb10919f098148f0d9",
+    "url": "/pokehoum-challenge/static/js/runtime~main.4acb1091.js"
+  },
+  {
+    "revision": "397e9719891d2b6f8abf1981b3a68ec9",
+    "url": "/pokehoum-challenge/index.html"
+  }
+];
